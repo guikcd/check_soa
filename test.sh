e4c6c019a8560ff10b2739a037613f4305044bff
Guillaume Delacour <gui@iroqwa.org>
2016-02-04 14:09:24 +0100

    Call check_soa.py due to renaming and document 'sudo iptables' use

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# test check_soa return codes
+# test check_soa.py return codes
+# use sudo and iptables to simulate unreachable ns
+# flush all iptables rules at the end
 
-stderr=$(mktemp --suffix=.test_check_soa)
+which sudo > /dev/null 2>&1 || exit 1
+which iptables > /dev/null 2>&1 || exit 1
 
-./check_soa iroqwa.org --verbose > $stderr 2>&1
+stderr=$(mktemp --suffix=.test_check_soa.py)
+
+./check_soa.py iroqwa.org --verbose > $stderr 2>&1
 if [ $? -ne 0 ]
 then
 	echo "TEST FAILED: existent domain don't return 0, output:"
@@ -14,7 +19,7 @@ else
 	echo "OK: existent domain return 0"
 fi
 
-./check_soa nonexistentdomain.tld --verbose > $stderr 2>&1
+./check_soa.py nonexistentdomain.tld --verbose > $stderr 2>&1
 if [ $? -ne 2 ]
 then
 	echo "TEST FAILED: nonexistent domain don't return 1, output:"
@@ -25,11 +30,11 @@ else
 	echo "OK: nonexistent domain return 1"
 fi
 
-# simulate broken ns
+echo "Simulate broken ns (thanks 'sudo iptables')"
 sudo iptables --flush
 sudo iptables --append OUTPUT --destination ns2.iroqwa.org --jump DROP
 
-./check_soa iroqwa.org --verbose > $stderr 2>&1
+./check_soa.py iroqwa.org --verbose > $stderr 2>&1
 if [ $? -ne 2 ]
 then
 	echo "TEST FAILED: existent domain with unreachable ns don't return 1, output:"
@@ -41,4 +46,5 @@ else
 fi
 
 rm --force $stderr
+echo "Flushing iptables "
 sudo iptables --flush
